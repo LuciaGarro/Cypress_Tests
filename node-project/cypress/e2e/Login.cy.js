886f60ac59993452b1a783812a62ac668fe0af41
LuciaGarro <garrolucia@gmail.com>
2024-09-25 20:18:42 -0300

    Login test updated

--- node-project/cypress/e2e/Login.cy.js
+++ node-project/cypress/e2e/Login.cy.js
@@ -1,5 +1,5 @@
 describe('Login test', () => {
   it('should visit the login page', () => {
-    cy.visit('https://admin-demo.nopcommerce.com/login?ReturnUrl=%2Fadmin%2F');
+    cy.visit('https://admin-demo.nopcommerce.com');
   })
 })
