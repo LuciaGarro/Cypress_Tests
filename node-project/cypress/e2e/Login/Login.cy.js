b537aec67eef23d602e96a833715eacb2bb124d7
LuciaGarro <garrolucia@gmail.com>
2024-09-27 14:37:15 -0300

    Login test updated

--- node-project/cypress/e2e/Login/Login.cy.js
+++ node-project/cypress/e2e/Login/Login.cy.js
@@ -1,6 +1,6 @@
 describe('Login test', () => {
   beforeEach(()  => {
-    cy.log("Visiting page");
+    cy.log("Visit page");
     cy.visit('https://admin-demo.nopcommerce.com/login');
   })
   it('visit the login page', () => {
@@ -20,8 +20,8 @@ describe('Login test', () => {
     cy.log("Click on password field")
     cy.get("#Password").type("Aa123456")
 
-    cy.log("Click on login field")
-    cy.get("body > div.master-wrapper-page > div > div > div > div > div.page-body > div.customer-blocks > div > form > div.buttons > button").click()
+    cy.log("Click on login button")
+    cy.get(".login-button").click()
  
   })
 })
